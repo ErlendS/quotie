f37232ddde9d247f9404647fedd62622d47dabfb
ErlendS <erlend.seppola@gmail.com>
2019-04-13 16:11:11 +0200

    push notification started

--- App.js
+++ App.js
@@ -7,9 +7,55 @@ import {
   TouchableOpacity,
   DatePickerIOS
 } from "react-native";
+import { Permissions, Notifications } from "expo";
+
 import { Icon } from "react-native-elements";
 import * as dateFns from "date-fns";
 
+const PUSH_ENDPOINT = "https://your-server.com/users/push-token";
+
+async function registerForPushNotificationsAsync() {
+  const { status: existingStatus } = await Permissions.getAsync(
+    Permissions.NOTIFICATIONS
+  );
+  let finalStatus = existingStatus;
+
+  // only ask if permissions have not already been determined, because
+  // iOS won't necessarily prompt the user a second time.
+  if (existingStatus !== "granted") {
+    // Android remote notification permissions are granted during the app
+    // install, so this will only ask on iOS
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    finalStatus = status;
+  }
+
+  // Stop here if the user did not grant permissions
+  if (finalStatus !== "granted") {
+    return;
+  }
+
+  // Get the token that uniquely identifies this device
+  let token = await Notifications.getExpoPushTokenAsync();
+
+  // POST the token to your backend server from where you can retrieve it to send push notifications.
+
+  // ADD FIREBASE ENDPOINT, SET UP BODY WITH CORRECT DATA
+  return fetch(PUSH_ENDPOINT, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      token: {
+        value: token
+      },
+      user: {
+        username: "Brent"
+      }
+    })
+  });
+}
 export default class App extends React.Component {
   state = {
     screen: "home",
@@ -287,14 +333,7 @@ export class FrequencySelector extends React.Component {
         </View>
         <PrimaryButton
           text={"Set Reminder"}
-          onPress={() =>
-            console.log(
-              "the state is ====",
-              this.state.startTime,
-              "and end time is =====",
-              this.state.endTime
-            )
-          }
+          onPress={() => registerForPushNotificationsAsync()}
         />
       </View>
     );
