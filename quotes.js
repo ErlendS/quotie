356243a0a30c3b909eb98a5fc6eabb9e62369c26
ErlendS <erlend.seppola@gmail.com>
2019-04-14 16:51:08 +0200

    quotes file

---
+++ quotes.js
@@ -0,0 +1,500 @@
+const quotes = [
+  {
+    author: "Epictetus",
+    text: `Make the best use of what is in your power, and take the rest as it happens.`
+  },
+
+  { author: "Epictetus", text: `No man is free who is not master of himself.` },
+
+  {
+    author: "Epictetus",
+    text: `Most of what passes for legitimate entertainment is inferior or foolish and only caters to or exploits people's weaknesses. Avoid being one of the mob who indulges in such pastimes. Your life is too short and you have important things to do. Be discriminating about what images and ideas you permit into your mind. If you yourself don't choose what thoughts and images you expose yourself to, someone else will, and their motives may not be the highest. It is the easiest thing in the world to slide imperceptibly into vulgarity. But there's no need for that to happen if you determine not to waste your time and attention on mindless pap.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `Remember, it is not enough to be hit or insulted to be harmed, you must believe that you are being harmed. If someone succeeds in provoking you, realize that your mind is complicit in the provocation. Which is why it is essential that we not respond impulsively to impressions; take a moment before reacting, and you will find it easier to maintain control.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `Men are disturbed not by things, but by the view which they take of them.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `Know, first, who you are, and then adorn yourself accordingly.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `It's not what happens to you, but how you react to it that matters.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `He is a wise man who does not grieve for the things which he has not, but rejoices for those which he has.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `If you want to improve, be content to be thought foolish and stupid.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `First say to yourself what you would be; and then do what you have to do.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `It is impossible for a man to learn what he thinks he already knows.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `Who is your master? Anyone who has control over things upon which you’ve set your heart, or over things which you seek to avoid.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `You are not your body and hair-style, but your capacity for choosing well. If your choices are beautiful, so too will you be.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `Now is the time to get serious about living your ideals. How long can you afford to put off who you really want to be? Your nobler self cannot wait any longer. Put your principles into practice – now. Stop the excuses and the procrastination. This is your life! You aren’t a child anymore. The sooner you set yourself to your spiritual program, the happier you will be. The longer you wait, the more you’ll be vulnerable to mediocrity and feel filled with shame and regret, because you know you are capable of better. From this instant on, vow to stop disappointing yourself. Separate yourself from the mob. Decide to be extraordinary and do what you need to do – now.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `Circumstances don’t make the man, they only reveal him to himself.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `We are not privy to the stories behind people’s actions, so we should be patient with others and suspend judgement of them, recognizing the limits of our understanding.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `Deliberate much before saying or doing anything, for you will not have the power of recalling what is said or done.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `It isn’t the events themselves that disturb people, but only their judgements about them.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `If you would be a reader, read; if a writer, write.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `A ship should not ride on a single anchor, nor life on a single hope.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `To make the best of what is in our power, and take the rest as it occurs.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `Wealth consists not in having great possessions, but in having few wants.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `If anyone tells you that a certain person speaks ill of you, do not make excuses about what is said of you but answer, "He was ignorant of my other faults, else he would not have mentioned these alone.”`
+  },
+
+  { author: "Epictetus", text: `Don't explain your philosophy. Embody it.` },
+
+  {
+    author: "Epictetus",
+    text: `There is only one way to happiness and that is to cease worrying about things which are beyond the power or our will. `
+  },
+
+  {
+    author: "Epictetus",
+    text: `Man is not worried by real problems so much as by his imagined anxieties about real problems.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `Don't just say you have read books. Show that through them you have learned to think better, to be a more discriminating and reflective person.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `The key is to keep company only with people who uplift you, whose presence calls forth your best.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `Any person capable of angering you becomes your master; he can anger you only when you permit yourself to be disturbed by him.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `He who laughs at himself never runs out of things to laugh at.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `Freedom is the only worthy goal in life. It is won by disregarding things that lie beyond our control.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `Other people's views and troubles can be contagious. Don't sabotage yourself by unwittingly adopting negative, unproductive attitudes through your associations with others.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `To accuse others for one's own misfortune is a sign of want of education. To accuse oneself shows that one's education has begun. To accuse neither oneself nor others shows that one's education is complete.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `I laugh at those who think they can damage me. They do not know who I am, they do not know what I think, they cannot even touch the things which are really mine and with which I live.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `It is not so much what happens to you as how you think about what happens.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `You are a little soul carrying around a corpse.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `Events do not just happen, but arrive by appointment.`
+  },
+
+  { author: "Epictetus", text: `You become what you give your attention to.` },
+
+  {
+    author: "Epictetus",
+    text: `Whoever is going to listen to the philosophers needs a considerable practice in listening.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `Personal merit cannot be derived from an external source. It is not to be found in your personal associations, nor can it be found in the regard of other people. It is a fact of life that other people, even people who love you, will not necessarily agree with your ideas, understand you, or share your enthusiasms.`
+  },
+
+  { author: "Epictetus", text: `No great thing is created suddenly.` },
+
+  {
+    author: "Epictetus",
+    text: `Never depend on the admiration of others. There is no strength in it. `
+  },
+
+  {
+    author: "Epictetus",
+    text: `Know you not that a good man does nothing for appearance sake, but for the sake of having done right?`
+  },
+
+  {
+    author: "Epictetus",
+    text: `Difficulty shows what men are. Therefore when a difficulty falls upon you, remember that God, like a trainer of wrestlers, has matched you with a rough young man. Why? So that you may become an Olympic conqueror; but it is not accomplished without sweat.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `Attach yourself to what is spiritually superior, regardless of what other people think or do. Hold to your true aspirations no matter what is going on around you.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `The greater the difficulty, the more glory in surmounting it. Skillful pilots gain their reputation from storms and tempests.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `Small-minded people blame others. Average people blame themselves. The wise see all blame as foolishness`
+  },
+
+  {
+    author: "Epictetus",
+    text: `Don't seek to have events happen as you wish, but wish them to happen as they do happen, and all will be well with you.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `It is unrealistc to expect people to see you as you see yourself.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `If you wish to be good, first believe that you are bad.`
+  },
+
+  {
+    author: "Epictetus",
+    text: `It is better to do wrong seldom and to own it, and to act right for the most part, than seldom to admit that you have done wrong and to do wrong often.`
+  },
+
+  {
+    author: "Seneca",
+    text: "Luck is what happens when preparation meets opportunity."
+  },
+
+  { author: "Seneca", text: "All cruelty springs from weakness." },
+
+  {
+    author: "Seneca",
+    text:
+      "Religion is regarded by the common people as true, by the wise as false, and by rulers as useful."
+  },
+
+  {
+    author: "Seneca",
+    text: "Difficulties strengthen the mind, as labor does the body."
+  },
+
+  {
+    author: "Seneca",
+    text:
+      "As is a tale, so is life: not how long it is, but how good it is, is what matters."
+  },
+
+  {
+    author: "Seneca",
+    text: "We suffer more often in imagination than in reality"
+  },
+
+  {
+    author: "Seneca",
+    text:
+      "Hang on to your youthful enthusiasms -- you’ll be able to use them better when you’re older."
+  },
+
+  {
+    author: "Seneca",
+    text:
+      "You act like mortals in all that you fear, and like immortals in all that you desire."
+  },
+
+  {
+    author: "Seneca",
+    text:
+      "Begin at once to live, and count each separate day as a separate life."
+  },
+
+  { author: "Seneca", text: "No man was ever wise by chance." },
+
+  {
+    author: "Seneca",
+    text:
+      "They lose the day in expectation of the night, and the night in fear of the dawn."
+  },
+
+  {
+    author: "Seneca",
+    text:
+      "If you really want to escape the things that harass you, what you’re needing is not to be in a different place but to be a different person."
+  },
+
+  {
+    author: "Seneca",
+    text: "Associate with people who are likely to improve you."
+  },
+
+  {
+    author: "Seneca",
+    text: "He suffers more than necessary, who suffers before it is necessary."
+  },
+
+  { author: "Seneca", text: "Only time can heal what reason cannot." },
+
+  { author: "Seneca", text: "As long as you live, keep learning how to live." },
+
+  { author: "Seneca", text: "The sun also shines on the wicked." },
+
+  {
+    author: "Seneca",
+    text:
+      "A gift consists not in what is done or given, but in the intention of the giver or doer."
+  },
+
+  {
+    author: "Seneca",
+    text:
+      "Often a very old man has no other proof of his long life than his age."
+  },
+
+  {
+    author: "Seneca",
+    text: "Nothing is more honorable than a grateful heart."
+  },
+
+  { author: "Seneca", text: "To wish to be well is a part of becoming well." },
+
+  { author: "Seneca", text: "There is no genius without a touch of madness." },
+
+  { author: "Seneca", text: "While we wait for life, life passes." },
+
+  {
+    author: "Seneca",
+    text:
+      "It is difficult to bring people to goodness with lessons, but it is easy to do so by example."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "Dwell on the beauty of life. Watch the stars, and see yourself running with them."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "You have power over your mind - not outside events. Realize this, and you will find strength."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "The happiness of your life depends upon the quality of your thoughts."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "Everything we hear is an opinion, not a fact. Everything we see is a perspective, not the truth."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text: "Waste no more time arguing about what a good man should be. Be one."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "When you arise in the morning think of what a privilege it is to be alive, to think, to enjoy, to love ..."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "If you are distressed by anything external, the pain is not due to the thing itself, but to your estimate of it; and this you have the power to revoke at any moment."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text: "The best revenge is to be unlike him who performed the injury."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text: "The soul becomes dyed with the colour of its thoughts."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "It is not death that a man should fear, but he should fear never beginning to live."
+  },
+
+  { author: "Marcus Aurelius", text: "Our life is what our thoughts make it." },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "If someone is able to show me that what I think or do is not right, I will happily change, for I seek the truth, by which no one was ever truly harmed. It is the person who continues in his self-deception and ignorance who is harmed."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "Very little is needed to make a happy life; it is all within yourself in your way of thinking."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "I have often wondered how it is that every man loves himself more than all the rest of men, but yet sets less value on his own opinion of himself than on the opinion of others."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "Whenever you are about to find fault with someone, ask yourself the following question: What fault of mine most nearly resembles the one I am about to criticize?"
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "When another blames you or hates you, or people voice similar criticisms, go to their souls, penetrate inside and see what sort of people they are. You will realize that there is no need to be racked with anxiety that they should hold any particular opinion about you."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "Do not act as if you were going to live ten thousand years. Death hangs over you. While you live, while it is in your power, be good."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "Here is a rule to remember in future, when anything tempts you to feel bitter: not `This is misfortune,` but `To bear this worthily is good fortune.`"
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "How much time he gains who does not look to see what his neighbour says or does or thinks, but only at what he does himself, to make it just and holy."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "You are a little soul carrying about a corpse, as Epictetus used to say."
+  },
+
+  { author: "Marcus Aurelius", text: "What we do now echoes in eternity." },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "Think of yourself as dead. You have lived your life. Now, take what's left and live it properly. What doesn't transmit light creates its own darkness."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "The impediment to action advances action. What stands in the way becomes the way."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "How ridiculous and how strange to be surprised at anything which happens in life"
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text: "Life is neither good or evil, but only a place for good and evil."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "Your days are numbered. Use them to throw open the windows of your soul to the sun. If you do not, the sun will soon set, and you with it."
+  },
+
+  {
+    author: "Marcus Aurelius",
+    text:
+      "Because a thing seems difficult for you, do not think it impossible for anyone to accomplish."
+  }
+];
